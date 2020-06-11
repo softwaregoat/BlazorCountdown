230b4bbd910000ba6badbc048a6a5c1bf03d8eb4
softwaregoat <softwaregoat1@gmail.com>
2020-06-11 14:57:12 +0100

    Change flip color

--- BlazorCountDown/BlazorCountDown/Models/Drawing.cs
+++ BlazorCountDown/BlazorCountDown/Models/Drawing.cs
@@ -8,11 +8,33 @@ namespace BlazorCountDown.Models
     public class Drawing
     {
         public int ID;
+        public int days;
+        public int hours;
+        public int minutes;
+        public int seconds;
         public DateTime time = new DateTime();
+
         public Drawing(int ID, DateTime time)
         {
             this.ID = ID;
             this.time = time;
+            GetValues();
+        }
+        public void GetValues()
+        {
+            var countDownDate = this.time;
+            var now = DateTime.Now;
+            var distance = countDownDate.Subtract(now);
+
+            var days = distance.Days;
+            var hours = distance.Hours;
+            var minutes = distance.Minutes;
+            var seconds = distance.Seconds;
+
+            this.days = days;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
         }
     }
 }
