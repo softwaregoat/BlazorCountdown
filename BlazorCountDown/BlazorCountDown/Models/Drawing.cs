c9e9fa02705021a28df7c6ad8070d39bdea7c8f3
softwaregoat <softwaregoat1@gmail.com>
2020-06-11 02:14:22 +0100

    init
    Initial Blazor countdown page

---
+++ BlazorCountDown/BlazorCountDown/Models/Drawing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCountDown.Models
+{
+    public class Drawing
+    {
+        public int ID;
+        public DateTime time = new DateTime();
+        public Drawing(int ID, DateTime time)
+        {
+            this.ID = ID;
+            this.time = time;
+        }
+    }
+}
